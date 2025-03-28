53763b07b662c621bcd7aa7d1aa4d7acbcaae2bb
neuralwavesignal <neuralwavesignal@gmail.com>
2025-03-28 23:35:18 +0530

    added supabase

--- stockscreener/settings.py
+++ stockscreener/settings.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,11 +69,19 @@ WSGI_APPLICATION = 'stockscreener.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),  # Fetching from environment variables
+        conn_max_age=600,
+        ssl_require=True  # Ensuring secure connection
+    )
 }
 
 
