9443b753235136a881b6766b2f79a193eb6b4fe0
neuralwavesignal <neuralwavesignal@gmail.com>
2025-03-28 22:46:43 +0530

    added static root to url

--- stockscreener/urls.py
+++ stockscreener/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
@@ -8,3 +11,6 @@ urlpatterns = [
     path('', include('screeners.urls')),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
+# Serve static files only in DEBUG mode
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
